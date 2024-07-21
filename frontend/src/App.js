64dc8c246c250094eb7cbf13850b59983a85d3ec
satish1404 <satishthakare2005@gmail.com>
2024-07-21 13:51:17 +0530

    adding files

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>0</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default App;
